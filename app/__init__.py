dbb4146fb1e30e77dae4f3cc59d4ebba3df38191
sk04237 <g2342049@tcu.ac.jp>
2025-01-18 23:59:42 +0900

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+# .envファイルをロード
+load_dotenv()
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        from .routes import main
+        app.register_blueprint(main)
+
+    return app
