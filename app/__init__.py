790a5a76a3cb0872e2ef70fd2d141d3f680d8db5
sk04237 <g2342049@tcu.ac.jp>
2025-01-19 01:51:28 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
-# .envファイルをロード
 load_dotenv()
 
 db = SQLAlchemy()
@@ -12,11 +11,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
 
     db.init_app(app)
 
-    with app.app_context():
-        from .app import main
-        app.register_blueprint(main)
+    # Blueprintの登録
+    from .app import main
+    app.register_blueprint(main)
 
     return app
